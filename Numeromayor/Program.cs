20c674925a745ad6014ae8747061a5692b3024bd
JohnMa17 <johnelmenorgv15@gmail.com>
2024-08-15 23:29:08 -0400

    Agregar archivos de proyecto.

---
+++ Numeromayor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace numeromayorymenor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es un programa para saber que numero es mayor o menor\n ");
+            int num1, num2, num3;
+
+            Console.WriteLine("Diga el primer numero\n ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Diga el segundo numero\n ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Diga el tercer numero\n ");
+            num3 = int.Parse(Console.ReadLine());
+
+            int mayor = num1;
+            int menor = num1;
+
+
+            if (num2 > mayor)
+                mayor = num2;
+
+            if (num3 > mayor)
+                mayor = num3;
+
+
+            if (num2 < menor)
+                menor = num2;
+            if (num3 < menor)
+                menor = num3;
+
+            Console.WriteLine("El numero mayor es: " + mayor);
+            Console.WriteLine("El numero menor es: " + menor);
+
+        }
+    }
+}
